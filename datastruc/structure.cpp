f93a1e21aa2fcf79eb31d7998d4f16dd7d5c1ede
mylaptop <jimkuoforever20@gmail.com>
2022-10-03 10:55:25 +0800

    create file

---
+++ datastruc/structure.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+#include "gcd.h"
+using namespace std;
---
+++ datastruc/structure.h
@@ -0,0 +1,12 @@
+#ifndef STRUCTURE_H_INCLUDED
+#define STRUCTURE_H_INCLUDED
+#include <iostream>
+#include "gcd.h"
+using namespace std;
+
+
+
+
+
+
+#endif // STRUCTURE_H_INCLUDED
